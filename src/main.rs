1dc253618cdf57489bb942fb4bb4b83bcd0f5f23
danxexe <danx.exe@gmail.com>
2022-10-25 00:12:28 -0300

    v2 heron + bevy 0.8

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
 use bevy::prelude::*;
+use bevy::window::*;
 use bevy_prototype_lyon::prelude::*;
 use bevy::input::mouse::*;
 use rand::{Rng,SeedableRng};
 use rand::rngs::StdRng;
-use bevy_rapier2d::prelude::*;
-use bevy_rapier2d::rapier::na::Vector2;
+use heron::prelude::*;
+use bevy_egui::{egui, EguiContext, EguiPlugin};
 
 #[derive(Component)]
 struct Player;
-
-#[derive(Component)]
-struct Glue(Entity);
 
 #[derive(Component)]
 struct Follow(Entity);
@@ -20,154 +18,158 @@ struct Attach(Entity);
 
 #[derive(Component)]
 struct Sphere {
+    pub color: Color,
     pub radius: f32,
-    pub border_radius: f32,
-    pub fill_color: Color,
-    pub border_color: Color,
+    pub border: f32,
 }
 
-pub struct HelloPlugin;
+#[derive(PhysicsLayer)]
+enum Layer {
+    Player,
+    Enemy,
+}
 
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app
-            .insert_resource(Msaa { samples: 4 })
-            .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
-            .add_startup_system(setup);
-    }
+#[derive(Default)]
+struct EditorState {
+    pub entity_id: String,
 }
 
 fn main() {
     App::new()
         .insert_resource(WindowDescriptor {
             title: "Strange Quark".to_string(),
-            width: 800 as f32,
-            height: 600 as f32,
-            position: Vec2::new(0.0, 0.0).into(),
+            mode: WindowMode::BorderlessFullscreen,
+            // width: 800 as f32,
+            // height: 600 as f32,
+            position: WindowPosition::At(Vec2::new(0.0, 0.0)),
             ..Default::default()
         })
+        .insert_resource(Msaa { samples: 4 })
+        .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
+        .insert_resource(EditorState { entity_id: "10".into() })
         .add_plugins(DefaultPlugins)
-        .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
-        .add_plugin(RapierRenderPlugin)
+        .add_plugin(EguiPlugin)
+        .add_plugin(PhysicsPlugin::default())
         .add_plugin(ShapePlugin)
-        .add_plugin(HelloPlugin)
+        .add_system(bevy::window::close_on_esc)
+        .add_startup_system(setup)
+        .add_startup_system_to_stage(StartupStage::PostStartup, setup_shape)
+        .add_startup_system_to_stage(StartupStage::PostStartup, setup_physics)
+        .add_system(gui)
         .add_system(zoom_camera)
         .add_system(keyboard_input_system)
         .add_system(handle_collisions)
-        .add_system_to_stage(CoreStage::Last, debug_position)
-        .add_system_to_stage(CoreStage::Update, on_attach)
+        // .add_system_to_stage(CoreStage::Last, debug_position)
         .run();
 }
 
-fn setup(mut commands: Commands, mut rapier_config: ResMut<RapierConfiguration>) {
-    rapier_config.gravity = Vector2::zeros();
-
-    let fill_color = Color::GRAY;
-    let stroke_color = Color::WHITE;
-    let radius = 50.0;
-    let stroke = radius / 10.0;
-
+fn setup(
+    mut commands: Commands,
+) {
     let player = commands
-    .spawn()
-    .insert(Sphere {
-        radius: radius,
-        border_radius: radius / 10.0,
-        fill_color: fill_color,
-        border_color: stroke_color,
-    })
-    .insert_bundle(GeometryBuilder::build_as(
-        &shapes::Circle {
-            radius: radius,
-            center: Vec2::ZERO,
-        },
-        DrawMode::Outlined {
-            fill_mode: FillMode::color(fill_color),
-            outline_mode: StrokeMode::new(stroke_color, stroke),
-        },
-        Transform {
-            scale: Vec3::new(1.0, 1.0, 1.0),
-            translation: Vec3::new(0.0, 0.0, 1.1),
+        .spawn()
+        .insert(Player)
+        .insert(Sphere {
+            radius: 50.0,
+            border: 50.0 / 10.0,
+            color: Color::GRAY,
+        })
+        .insert(Transform {
+            translation: Vec3::new(0.0, 0.0, 1.0),
             ..Transform::default()
-        },
-    ))
-    .insert_bundle(ColliderBundle {
-        position: Vec2::new(0.0, 0.0).into(),
-        shape: ColliderShape::ball(radius).into(),
-        flags: (ActiveEvents::CONTACT_EVENTS).into(),
-        ..Default::default()
-    })
-    .insert_bundle(RigidBodyBundle {
-        position: Vec2::new(0.0, 0.0).into(),
-        ..Default::default()
-    })
-    // .insert(ColliderPositionSync::Discrete)
-    .insert(RigidBodyPositionSync::Discrete)
-    // .insert(ColliderDebugRender::with_id(1))
-    .insert(Player)
-    // .push_children(&[child])
-    .id();
+        })
+        .insert(CollisionLayers::new(Layer::Player, Layer::Enemy))
+        .id();
 
-    commands.entity(player)
-        .insert(Glue(player));
-
-    commands.spawn_bundle(OrthographicCameraBundle::new_2d())
+    commands.spawn_bundle(Camera2dBundle::default())
         .insert(Follow(player));
 
-    let mut rnd = StdRng::seed_from_u64(2);
+    let mut rnd = StdRng::seed_from_u64(1);
 
-    for _i in 0..200 {
+    for _i in 0..1000 {
         let hue: f32 = rnd.gen_range(0.0..=360.0);
-        let fill_color = Color::hsla(hue, 0.4, 0.3, 1.0);
-        let stroke_color = Color::hsla(hue, 0.5, 0.4, 0.9);
+        let color = Color::hsla(hue, 0.4, 0.3, 1.0);
         let radius = rnd.gen_range(5.0..200.0);
-        let stroke = radius / 10.0;
-        let x: f32 = rnd.gen_range(-4000.0..=4000.0);
-        let y: f32 = rnd.gen_range(-3000.0..=4000.0);
+        let x: f32 = rnd.gen_range(-10000.0..=10000.0);
+        let y: f32 = rnd.gen_range(-10000.0..=10000.0);
     
         commands
-        .spawn()
-        .insert(Sphere {
-            radius: radius,
-            border_radius: stroke,
-            fill_color: fill_color,
-            border_color: stroke_color,
-        })    
-        .insert_bundle(GeometryBuilder::build_as(
-            &shapes::Circle {
+            .spawn()
+            .insert(Sphere {
                 radius: radius,
-                center: Vec2::ZERO,
-            },
-            DrawMode::Outlined {
-                fill_mode: FillMode::color(fill_color),
-                outline_mode: StrokeMode::new(stroke_color, stroke),
-            },
-            Transform {
-                scale: Vec3::new(1.0, 1.0, 1.0),
+                border: radius / 10.0,
+                color: color,
+            })
+            .insert(Transform {
                 translation: Vec3::new(x, y, 1.0),
                 ..Transform::default()
-            },
-        ))
-        .insert_bundle(ColliderBundle {
-            position: Vec2::new(x, y).into(),
-            shape: ColliderShape::ball(radius).into(),
-            ..Default::default()
-        })
-        .insert_bundle(RigidBodyBundle {
-            position: Vec2::new(x, y).into(),
-            ..Default::default()
-        })
-        .insert(ColliderPositionSync::Discrete)
-        // .insert(RigidBodyPositionSync::Discrete)
-        // .insert(ColliderDebugRender::with_id(2))
-        ;
+            })
+            .insert(CollisionLayers::new(Layer::Enemy, Layer::Player).with_mask(Layer::Enemy))
+            ;
     }
+}
+
+fn setup_shape(
+    query: Query<(Entity, &Sphere, &Transform)>,
+    mut commands: Commands,
+) {
+    for (entity, sphere, transform) in query.iter() {
+        commands.entity(entity)
+            .insert_bundle(GeometryBuilder::build_as(
+                &shapes::Circle {
+                    radius: sphere.radius,
+                    center: Vec2::ZERO,
+                },
+                DrawMode::Outlined {
+                    fill_mode: FillMode::color(sphere.color),
+                    outline_mode: StrokeMode::new(sphere.color + Color::hsla(0.0, 0.0, 0.5, -0.1), sphere.border),
+                },
+                *transform,
+            ));
+    }
+}
+
+fn setup_physics(
+    query: Query<(Entity, &Sphere)>,
+    mut commands: Commands,
+) {
+    for (entity, sphere) in query.iter() {
+        commands.entity(entity)
+            .insert(RigidBody::Dynamic)
+            .insert(CollisionShape::Sphere { radius: sphere.radius })
+            .insert(Velocity::default());
+    }
+}
+
+fn gui(
+    mut egui_context: ResMut<EguiContext>,
+    mut editor_state: ResMut<EditorState>,
+    query: Query<&GlobalTransform>
+) {
+    egui::Window::new("debug").show(egui_context.ctx_mut(), |ui| {
+        let id = editor_state.entity_id.parse::<u32>()
+            .or(Err(bevy::ecs::query::QueryEntityError::NoSuchEntity))
+            .and_then(|id| Ok(Entity::from_raw(id)))
+            .and_then(|entity| Ok(query.get(entity)));
+
+        ui.label("entity");
+        ui.text_edit_singleline(&mut editor_state.entity_id);
+
+        ui.label("position");
+
+        if let Ok(Ok(obj)) = id {
+            ui.label(format!("{}", obj.translation()));
+        } else {
+            ui.label("no entity");
+        }
+    });
 }
 
 fn keyboard_input_system(
     keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&Player, &mut RigidBodyForcesComponent, &RigidBodyMassPropsComponent)>,
+    mut query: Query<&mut Velocity, With<Player>>,
 ) {
-    let mut direction = Vec2::ZERO;
+    let mut direction = Vec3::ZERO;
     if keyboard_input.pressed(KeyCode::A) {
         direction.x -= 1.0;
     }
@@ -184,8 +186,9 @@ fn keyboard_input_system(
         direction.y -= 1.0;
     }
 
-    for (_player, mut forces, mass) in query.iter_mut() {
-        forces.force = (direction * 128.0 * mass.mass()).into();
+    for mut velocity in query.iter_mut() {
+        velocity.linear = direction * 200.0;
+        velocity.angular = AxisAngle::new(Vec3::new(0.0, 0.0, 1.0), 1.0);
     }
 }
 
@@ -209,118 +212,58 @@ fn zoom_camera(
     }
 }
 
-// fn debug_position(query: Query<(&Player, &ColliderPositionComponent)>) {
-fn debug_position(query: Query<&ColliderPositionComponent>) {
-    // let e = Entity::from_raw(92);
-    // if let Ok(obj) = query.get(e) {
-    //     println!("{}", obj.translation);
-    // } else {
-    //     println!("nope");
-    // }
-}
-
-fn on_attach(
-    query: Query<(Entity, &Attach, &Sphere)>,
-    t_query: Query<&Transform>,
-    mut commands: Commands,
-) {
-    for (e, &Attach(player), sphere) in query.iter() {
-        println!("Attach! {}", e.id());
-
-        let parent_t = t_query.get(player).unwrap();
-        let child_t = t_query.get(e).unwrap();
-        let relative_t = Transform::from_matrix(parent_t.compute_matrix().inverse() * child_t.compute_matrix());
-
-        // let relative_t = parent_t.rotation * (child_t.translation.truncate() - parent_t.translation.truncate()).extend(0.0);
-        // let relative_t = (child_t.translation.truncate() - parent_t.translation.truncate()).extend(0.0);
-        println!("{:?}", relative_t);
-
-        commands.entity(e)
-            .despawn();
-
-        let new_e = commands
-            .spawn_bundle(GeometryBuilder::build_as(
-                &shapes::Circle {
-                    radius: sphere.radius,
-                    center: Vec2::ZERO,
-                },
-                DrawMode::Outlined {
-                    fill_mode: FillMode::color(sphere.fill_color),
-                    outline_mode: StrokeMode::new(sphere.border_color, sphere.border_radius),
-                },
-                relative_t,
-            ))
-            // .remove::<Attach>()
-            .insert_bundle(ColliderBundle {
-                position: relative_t.translation.into(),
-                shape: ColliderShape::ball(sphere.radius).into(),
-                flags: (ActiveEvents::CONTACT_EVENTS).into(),
-                ..Default::default()
-            })
-            .insert(ColliderPositionSync::Discrete)
-            // .insert(ColliderDebugRender::with_id(2))
-            .insert(Glue(player))
-            .id()
-            ;
-
-        commands.entity(player).push_children(&[new_e]);
+#[allow(dead_code)]
+fn debug_position(query: Query<&GlobalTransform>) {
+    let e = Entity::from_raw(10);
+    if let Ok(obj) = query.get(e) {
+        println!("{}", obj.translation());
+    } else {
+        // println!("nope");
     }
 }
 
 fn handle_collisions(
-    mut contact_events: EventReader<ContactEvent>,
     mut commands: Commands,
-    query: Query<&Glue>,
+    mut events: EventReader<CollisionEvent>,
+    mut query: Query<&mut Transform>,
 ) {
-    for event in contact_events.iter() {
-        match event {
-            ContactEvent::Started(a, b) => {
-                let (a, b) = (a.entity(), b.entity());
-                let (player, other) = match (query.get(a), query.get(b)) {
-                    (Ok(&Glue(player)), _) => (player, b),
-                    (_, Ok(&Glue(player))) => (player, a),
-                    _ => return,
-                };
+    events
+        .iter()
+        .filter(|e| e.is_started())
+        .filter_map(|event| {
+            let (entity_1, entity_2) = event.rigid_body_entities();
+            let (layers_1, layers_2) = event.collision_layers();
 
-                println!("collision: {}, {}", a.id(), b.id());
-                println!("player, other: {}, {}", player.id(), other.id());
+            if is_player(layers_1) && is_enemy(layers_2) {
+                Some((entity_1, entity_2))
+            } else if is_player(layers_2) && is_enemy(layers_1) {
+                Some((entity_1, entity_2))
+            } else {
+                None
+            }
+        })
+        .for_each(|(player, enemy)| {
+            commands.entity(enemy)
+                .remove::<RigidBody>()
+                .insert(CollisionLayers::new(Layer::Player, Layer::Enemy))
+                ;
 
-                commands.entity(other)
-                    // .despawn()
-                    .remove_bundle::<RigidBodyBundle>()
-                    .remove_bundle::<ColliderBundle>()
-                    // .remove::<ColliderPositionSync>()
-                    .insert(Attach(player))
-                    // .remove::<RigidBodyTypeComponent>()
-                    // .remove::<RigidBodyPositionComponent>()
-                    // .remove::<RigidBodyVelocityComponent>()
-                    // .remove::<RigidBodyMassPropsComponent>()
-                    // .remove::<RigidBodyForcesComponent>()
-                    // .remove::<RigidBodyActivationComponent>()
-                    // .remove::<RigidBodyDampingComponent>()
-                    // .remove::<RigidBodyDominanceComponent>()
-                    // .remove::<RigidBodyCcdComponent>()
-                    // .remove::<RigidBodyChangesComponent>()
-                    // .remove::<RigidBodyIdsComponent>()
-                    // .insert_bundle(ColliderBundle {
-                    //     position: ColliderPositionComponent(Vec2::new(0.0, 0.0).into()),
-                    //     shape: ColliderShape::ball(100.0).into(),
-                    //     ..Default::default()
-                    // })
-                    // .remove::<RigidBodyPositionSync>()
-                    // .insert(ColliderDebugRender::with_id(1))
-                    // .insert(ColliderPositionSync::Discrete)
-                    // .insert(ColliderParentComponent(ColliderParent {
-                    //     handle: player.handle(),
-                    //     pos_wrt_parent: Vec2::ZERO.into(),
-                    //     // pos_wrt_parent: Vec2::new(x, y).into(),
-                    // }))
-                    ;
-                // commands.entity(b.entity()).push_children(&[a.entity()]);
+            if let Ok([parent_t, mut child_t]) = query.get_many_mut([player, enemy]) {
+                let relative_t = Transform::from_matrix(parent_t.compute_matrix().inverse() * child_t.compute_matrix());
+                child_t.translation = relative_t.translation;
+                child_t.rotation = relative_t.rotation;
+                child_t.scale = relative_t.scale;
+            }
 
-                // println!("{}", other.id());
-            },
-            ContactEvent::Stopped(_a, _b) => (),
-        }
-    }
+            commands.entity(player)
+                .push_children(&[enemy]);
+        });
+}
+
+fn is_player(layers: CollisionLayers) -> bool {
+    layers.contains_group(Layer::Player) && !layers.contains_group(Layer::Enemy)
+}
+
+fn is_enemy(layers: CollisionLayers) -> bool {
+    !layers.contains_group(Layer::Player) && layers.contains_group(Layer::Enemy)
 }
